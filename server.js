1624491f9221446cd7380fe290bfed37f51d80d6
piitur <thanat.peter@mail.kmutt.ac.th>
2023-12-08 21:42:22 +0700

    Initial commit for backend 2

--- server.js
+++ server.js
@@ -1,16 +1,18 @@
 const express = require('express');
 const cors = require('cors');
-const mysql = require('mysql');
+const mysql = require('mysql2');
 
 const app = express();
 const port = 3001;
 
-app.use(cors()); // Add this line to enable CORS
+app.use(cors());
+app.use(express.json());
+
 
 const connection = mysql.createConnection({
   host: '10.4.85.33',
   user: 'root',
-  password: 'mysql', //mamp ใส้root sampt ไม่ใส่pw
+  password: 'mysql', 
   database: 'oze',
 });
 
@@ -22,14 +24,90 @@ connection.connect((err) => {
   }
 });
 
+// Get route
 app.get('/api/products', (req, res) => {
-  const query = 'SELECT * FROM products';
+  const query = 'SELECT * FROM Product';
+
   connection.query(query, (err, results) => {
     if (err) {
       console.error('Error executing MySQL query: ', err);
       res.status(500).json({ error: 'Internal Server Error' });
     } else {
       res.status(200).json(results);
+    }
+  });
+});
+
+// Create
+app.post('/api/products', (req, res) => {
+  // Extract product data from the request body
+  const { ProductName, Description, Price, StockQuantity, Color, IsTrend, IsNew, CategoryId, ImagePath } = req.body;
+
+  // Perform SQL INSERT query to add a new product
+  const query = `INSERT INTO Product (ProductName, Description, Price, StockQuantity, Color, IsTrend, IsNew, Category_category_id, ImagePath) 
+                 VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)`;
+  const values = [ProductName, Description, Price, StockQuantity, Color, IsTrend, IsNew, CategoryId, ImagePath];
+
+  connection.query(query, values, (err, results) => {
+    if (err) {
+      console.error('Error executing MySQL query: ', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      res.status(201).json({ message: 'Product created successfully' });
+    }
+  });
+});
+
+// Update
+app.put('/api/products/:id', (req, res) => {
+  const productId = req.params.id;
+  // Extract updated product data from the request body
+  const { ProductName, Description, Price, StockQuantity, Color, IsTrend, IsNew, CategoryId, ImagePath } = req.body;
+
+  // Perform SQL UPDATE query to modify the details of a product
+  const query = `UPDATE Product 
+                 SET ProductName=?, Description=?, Price=?, StockQuantity=?, Color=?, IsTrend=?, IsNew=?, Category_category_id=?, ImagePath=?
+                 WHERE ProductId=?`;
+  const values = [ProductName, Description, Price, StockQuantity, Color, IsTrend, IsNew, CategoryId, ImagePath, productId];
+
+  connection.query(query, values, (err, results) => {
+    if (err) {
+      console.error('Error executing MySQL query: ', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      res.status(200).json({ message: 'Product updated successfully' });
+    }
+  });
+});
+
+// Delete
+app.delete('/api/products/:id', (req, res) => {
+  const productId = req.params.id;
+
+  // Perform SQL DELETE query to remove a product
+  const query = 'DELETE FROM Product WHERE ProductId=?';
+
+  connection.query(query, [productId], (err, results) => {
+    if (err) {
+      console.error('Error executing MySQL query: ', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      res.status(200).json({ message: 'Product deleted successfully' });
+    }
+  });
+});
+
+// Add this route to your server.js
+app.get('/api/products/count', (req, res) => {
+  const query = 'SELECT COUNT(*) as count FROM Product';
+
+  connection.query(query, (err, results) => {
+    if (err) {
+      console.error('Error executing MySQL query: ', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      const count = results[0]?.count || 0;
+      res.status(200).json({ count });
     }
   });
 });
