72e23f5ed52624aef2ce7f42b1d7dc4be0e177c8
piitur <thanat.peter@mail.kmutt.ac.th>
2023-12-11 17:00:53 +0700

    be

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ app.use(express.json());
 
 
 const connection = mysql.createConnection({
-  host: '10.4.85.33',
-  user: 'root',
-  password: 'mysql', 
-  database: 'ozedb',
-  port: '3306',
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'mysql',
+  database: process.env.DB_NAME || 'ozedb',
+  port: process.env.DB_PORT || '3306',
 });
 
 connection.connect((err) => {
