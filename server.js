c611f83e54732059baaf99e47fba0ecf33816bed
Nithi Lampay <lingchinball@MacBook-Air-khxng-Nithi.local>
2024-03-19 20:28:29 +0700

    size

--- server.js
+++ server.js
@@ -647,7 +647,7 @@ app.delete('/api/cart/:id', express.json(), (req, res) => {
 
 /// payment
 app.post("/api/checkout", express.json(), async (req, res) => {
-  const { userDetail, total_amount, total_quantity, product, isQuickBuy , size } =
+  const { userDetail, total_amount, total_quantity, product, isQuickBuy  } =
     req.body;
   ///isQuickBuy คือซื้อจากหน้าแรก ไม่ใช่กระเป๋า
   try {
@@ -682,7 +682,7 @@ app.post("/api/checkout", express.json(), async (req, res) => {
       orderShowId
     );
 
-    await Promise.all(product.map((item) => addOrderItem(item, ordersId, size)));
+    await Promise.all(product.map((item) => addOrderItem(item, ordersId)));
 
     res.json({
       message: "Checkout success.",
@@ -760,13 +760,13 @@ function createOrder(
   });
 }
 
-function addOrderItem(item, ordersId, size) {
+function addOrderItem(item, ordersId) {
   return new Promise((resolve, reject) => {
     const query =
       "INSERT INTO OrderItems (price, Quantity, Order_OrderID, Product_ProductId, Size) VALUES (?, ?, ?, ?, ?)";
     connection.query(
       query,
-      [item.price, item.Quantity, ordersId, item.product_id, size],
+      [item.price, item.Quantity, ordersId, item.product_id, item.size],
       (err, results) => {
         if (err) reject(err);
         else resolve();
